51295b36187ab17ee3792fa8f92843fdf9596bfd
Matthew Feickert <matthew.feickert@cern.ch>
2019-05-21 12:31:54 -0500

    Apply Black code style to tests (#16)
    Apply the Black code style to the notebook tests
    
    c.f. https://github.com/ambv/black

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,24 +6,30 @@ def test_notebooks_all_execute(tmpdir):
     outputnb = tmpdir.join('output.ipynb')
     common_kwargs = {
         'output_path': str(outputnb),
-        'kernel_name': 'python{}'.format(sys.version_info.major)
+        'kernel_name': 'python{}'.format(sys.version_info.major),
     }
 
     pm.execute_notebook(
-        'Notebooks/Introductory/Chi-Squared-Distribution.ipynb', **common_kwargs)
+        'Notebooks/Introductory/Chi-Squared-Distribution.ipynb', **common_kwargs
+    )
+    pm.execute_notebook('Notebooks/Introductory/Error-on-means.ipynb', **common_kwargs)
     pm.execute_notebook(
-        'Notebooks/Introductory/Error-on-means.ipynb', **common_kwargs)
+        'Notebooks/Introductory/Gaussian-Distribution.ipynb', **common_kwargs
+    )
     pm.execute_notebook(
-        'Notebooks/Introductory/Gaussian-Distribution.ipynb', **common_kwargs)
+        'Notebooks/Introductory/Gaussian-Sampling.ipynb', **common_kwargs
+    )
     pm.execute_notebook(
-        'Notebooks/Introductory/Gaussian-Sampling.ipynb', **common_kwargs)
+        'Notebooks/Introductory/Likelihood-Function.ipynb', **common_kwargs
+    )
     pm.execute_notebook(
-        'Notebooks/Introductory/Likelihood-Function.ipynb', **common_kwargs)
-    pm.execute_notebook(
-        'Notebooks/Introductory/probability-integral-transform.ipynb', **common_kwargs)
+        'Notebooks/Introductory/probability-integral-transform.ipynb', **common_kwargs
+    )
 
     pm.execute_notebook(
-        'Notebooks/Likelihood_Methods/Extended-Likelihood.ipynb', **common_kwargs)
+        'Notebooks/Likelihood_Methods/Extended-Likelihood.ipynb', **common_kwargs
+    )
 
     pm.execute_notebook(
-        'Notebooks/Sampling/Rejection-Sampling-MC.ipynb', **common_kwargs)
+        'Notebooks/Sampling/Rejection-Sampling-MC.ipynb', **common_kwargs
+    )
