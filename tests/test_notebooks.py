ec8ced93ce173d042341cab5aec097c58ee4d820
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-17 15:08:15 -0500

    Update tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -3,33 +3,36 @@ import papermill as pm
 
 
 def test_notebooks_all_execute(tmpdir):
-    outputnb = tmpdir.join('output.ipynb')
+    outputnb = tmpdir.join("output.ipynb")
     common_kwargs = {
-        'output_path': str(outputnb),
-        'kernel_name': 'python{}'.format(sys.version_info.major),
+        "output_path": str(outputnb),
+        "kernel_name": "python{}".format(sys.version_info.major),
     }
 
     pm.execute_notebook(
-        'Notebooks/Introductory/Chi-Squared-Distribution.ipynb', **common_kwargs
-    )
-    pm.execute_notebook('Notebooks/Introductory/Error-on-means.ipynb', **common_kwargs)
-    pm.execute_notebook(
-        'Notebooks/Introductory/Gaussian-Distribution.ipynb', **common_kwargs
+        "book/notebooks/Introductory/Chi-Squared-Distribution.ipynb", **common_kwargs
     )
     pm.execute_notebook(
-        'Notebooks/Introductory/Gaussian-Sampling.ipynb', **common_kwargs
+        "book/notebooks/Introductory/Error-on-means.ipynb", **common_kwargs
     )
     pm.execute_notebook(
-        'Notebooks/Introductory/Likelihood-Function.ipynb', **common_kwargs
+        "book/notebooks/Introductory/Gaussian-Distribution.ipynb", **common_kwargs
     )
     pm.execute_notebook(
-        'Notebooks/Introductory/probability-integral-transform.ipynb', **common_kwargs
+        "book/notebooks/Introductory/Gaussian-Sampling.ipynb", **common_kwargs
+    )
+    pm.execute_notebook(
+        "book/notebooks/Introductory/Likelihood-Function.ipynb", **common_kwargs
+    )
+    pm.execute_notebook(
+        "book/notebooks/Introductory/probability-integral-transform.ipynb",
+        **common_kwargs
     )
 
     pm.execute_notebook(
-        'Notebooks/Likelihood_Methods/Extended-Likelihood.ipynb', **common_kwargs
+        "Notebooks/Likelihood_Methods/Extended-Likelihood.ipynb", **common_kwargs
     )
 
     pm.execute_notebook(
-        'Notebooks/Sampling/Rejection-Sampling-MC.ipynb', **common_kwargs
+        "Notebooks/Sampling/Rejection-Sampling-MC.ipynb", **common_kwargs
     )
