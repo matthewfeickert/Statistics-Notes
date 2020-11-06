aaab6b0c7f0001f951955ce95a8c0b31fc3f303f
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-05 22:34:49 -0600

    Fix tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,38 +1,59 @@
 import sys
 import papermill as pm
+import pytest
 
 
-def test_notebooks_all_execute(tmpdir):
+@pytest.fixture()
+def common_kwargs(tmpdir):
     outputnb = tmpdir.join("output.ipynb")
-    common_kwargs = {
+    return {
         "output_path": str(outputnb),
-        "kernel_name": "python{}".format(sys.version_info.major),
+        "kernel_name": f"python{sys.version_info.major}",
     }
 
-    pm.execute_notebook(
-        "book/notebooks/Introductory/Chi-Squared-Distribution.ipynb", **common_kwargs
-    )
-    pm.execute_notebook(
-        "book/notebooks/Introductory/Error-on-means.ipynb", **common_kwargs
-    )
-    pm.execute_notebook(
-        "book/notebooks/Introductory/Gaussian-Distribution.ipynb", **common_kwargs
-    )
-    pm.execute_notebook(
-        "book/notebooks/Introductory/Gaussian-Sampling.ipynb", **common_kwargs
-    )
+
+def test_likelihood_function(common_kwargs):
     pm.execute_notebook(
         "book/notebooks/Introductory/Likelihood-Function.ipynb", **common_kwargs
     )
+
+
+def test_error_on_means(common_kwargs):
+    pm.execute_notebook(
+        "book/notebooks/Introductory/Error-on-means.ipynb", **common_kwargs
+    )
+
+
+def test_gaussian_sampling(common_kwargs):
+    pm.execute_notebook(
+        "book/notebooks/Introductory/Gaussian-Sampling.ipynb", **common_kwargs
+    )
+
+
+def test_normal_dist(common_kwargs):
+    pm.execute_notebook(
+        "book/notebooks/Introductory/Gaussian-Distribution.ipynb", **common_kwargs
+    )
+
+
+def test_chi_square_dist(common_kwargs):
+    pm.execute_notebook(
+        "book/notebooks/Introductory/Chi-Squared-Distribution.ipynb", **common_kwargs
+    )
+
+
+def test_probability_integral_transform(common_kwargs):
     pm.execute_notebook(
         "book/notebooks/Introductory/probability-integral-transform.ipynb",
-        **common_kwargs
+        **common_kwargs,
     )
 
+
+def test_rejection_sampling(common_kwargs):
     pm.execute_notebook(
-        "Notebooks/Likelihood_Methods/Extended-Likelihood.ipynb", **common_kwargs
+        "book/notebooks/simulation/Rejection-Sampling-MC.ipynb", **common_kwargs
     )
 
-    pm.execute_notebook(
-        "Notebooks/Sampling/Rejection-Sampling-MC.ipynb", **common_kwargs
-    )
+
+def test_extended_likelihood(common_kwargs):
+    pm.execute_notebook("book/notebooks/HEP/Extended-Likelihood.ipynb", **common_kwargs)
